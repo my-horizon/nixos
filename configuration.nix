29eae1dd9b5d588e81d8ee18ba8e91f9c9e7e20d
my-horizon <mail@virtualhorizon.de>
2025-12-23 22:18:43 +0100

    changed file structure minorly

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,8 @@
     openjdk25
   ];
 
+  # List services that you want to enable:
+
   # Ly
   services.displayManager.ly = {
     enable = true;
@@ -114,8 +116,6 @@
       nerd-fonts.jetbrains-mono
     ];
   };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
